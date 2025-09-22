1a35c8a26692469541ec253cc156bb50322a3612
Aarush Dubey <mrknownunknown2702@gmail.com>
2025-09-22 23:52:20 +0530

    Add files via upload

---
+++ Stack/linkedlist/stackll.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// stack as adt using linked list
+typedef struct node
+{
+    int data;
+    struct node *next;
+} node;
+
+node *top = NULL;
+
+int main(void)
+{
+
+}
